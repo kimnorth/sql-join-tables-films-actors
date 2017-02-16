fe4babb34b02be4c5380ff9e9106422142e4bfb5
kimnorth <benignhumour@gmail.com>
2017-02-16 13:48:40 +0000

    Created database sql file

---
+++ db/films_actors.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS films_with_actors;
+DROP TABLE IF EXISTS films;
+DROP TABLE IF EXISTS actors;
+
+
+CREATE TABLE films (
+  film_id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  genre VARCHAR(255)
+);
+
+CREATE TABLE actors (
+  actor_id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  age INT4
+);
+
+CREATE TABLE films_with_actors (
+  id_films_actors SERIAL4 PRIMARY KEY,
+  film_id INT4 REFERENCES films(film_id) ON DELETE CASCADE,
+  actor_id INT4 REFERENCES actors(actor_id) ON DELETE CASCADE
+);
